78bdbfd0497b4893ae9ca7d7e75ce7811b008b72
Aditi <cs1140205@iitd.ac.in>
2017-11-05 21:45:49 +0530

    parsing

--- VQABase.py
+++ VQABase.py
@@ -7,8 +7,11 @@ import numpy as np
 import sys, nltk
 import os, argparse
 import time, random
+import torchvision.models as models
 
 word_embedding_dim = 200
+
+def  load_data
 
 class VQABaseline(nn.Module):
 	def __init__(self, o, k, s, hid):
@@ -20,12 +23,15 @@ class VQABaseline(nn.Module):
 		else:
 			self.dtype = torch.FloatTensor
 
+		self.vgg_pre = models.vgg19_bn(pre_trained=True)
+
 		self.image_conv1 = nn.Conv2d(3, o[0], k[0], s[0])
 		self.image_conv2 = nn.Conv2d(o[0], o[1], k[1], s[1])
 		self.image_conv3 = nn.Conv2d(o[1], o[2], k[2], s[2])
 
-		self.ques_lstm1 = nn.LSTM(word_embedding_dim, hid[0], num_layers=1, bidirectional=False, batch_first=True)
-		self.ques_lstm2 = nn.LSTM(hid[0], hid[1], num_layers=1, bidirectional=False, batch_first=True)
+		self.ques_lstm = nn.LSTM(word_embedding_dim, hid[0], num_layers=1, bidirectional=False, batch_first=True)
+		# self.ques_lstm2 = nn.LSTM(hid[0], hid[1], num_layers=1, bidirectional=False, batch_first=True)
+
 
 
 def get_arguments():
@@ -41,3 +47,6 @@ def get_arguments():
 
 	opts = parser.parse_args(sys.argv[1:])
 	return opts
+
+
+vgg_pre = models.vgg19_bn(pre_trained=True)
---
+++ parse.py
@@ -0,0 +1,47 @@
+import json
+
+def parse_ques(file_path):
+	ques_list = []
+	img_wise_ques = {}
+	with open(file_path, 'r') as f:
+		f_json = json.loads(f.read())
+		print "Task Type :", f_json["task_type"]
+		print "Info version :", f_json["info"]["version"]
+		ques_list = f_json["questions"]
+		for q in f_json["questions"]:
+			if q["image_id"] not in img_wise_ques:
+				img_wise_ques[q["image_id"]] = []
+				print "img id : ", q["image_id"]
+			img_wise_ques[q["image_id"]].append(q["question_id"])
+	print "Total ques :", len(ques_list)
+	for i in xrange(3):
+		k = int(input("Enter image id :"))
+		if k in img_wise_ques:
+			print "No of ques : ", img_wise_ques[k]
+		else:
+			print "IMg id doesn't exist"
+
+def parse_annotation(file_path):
+	annot_list = []
+	ques_wise_ans = {}
+	with open(file_path, 'r') as f:
+		f_json = json.loads(f.read())
+		print "Data Sub Type :", f_json["data_subtype"]
+		print "Info version :", f_json["info"]["version"]
+		annot_list = f_json["annotations"]
+		for ann in f_json["annotations"]:
+			if ann["question_id"] not in ques_wise_ans:
+				ques_wise_ans[ann["question_id"]] = []
+			for answer in ann["answers"]:
+				ques_wise_ans[ann["question_id"]].append(answer)
+	print "Total annotations :", len(annot_list)
+	for i in xrange(5):
+		k = int(input("Enter ques id :"))
+		if k in ques_wise_ans:
+			print "#ans ", ques_wise_ans[k]
+		else:
+			print "Invalid"
+
+
+parse_ques("v2_OpenEnded_mscoco_val2014_questions.json")
+parse_annotation("v2_mscoco_val2014_annotations.json")
