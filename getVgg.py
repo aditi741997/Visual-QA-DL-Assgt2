55111c09e825f3b494040177867d01fb8b301ffe
Praveen Kulkarni <praveenkulkarni1996@gmail.com>
2017-11-12 00:34:58 +0530

    add code to preprocess vgg outputs [pk]
    The files will be written to "../Data/<foldername>_vgg/<filename>.dat"

--- getVgg.py
+++ getVgg.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import torch
 import torch.autograd as autograd
 import torchvision.transforms as transforms
@@ -12,39 +13,39 @@ import time, random
 import torchvision.models as models
 import pickle
 from scipy import misc
+from skimage import io
 from PIL import Image
+from pprint import pprint
 
 vgg_model = models.vgg19_bn(pretrained=True)
-# vgg_model.classifier = nn.Sequential(*(vgg_model.classifier[i] for i in xrange(4)))
+# vgg_model = nn.Sequential(*list(real_vgg_model.features.children())[:-1])
 
 def get_vgg(folder):
-	new_folder = folder + "_vgg"
-	i = 0
-	for img in os.listdir(folder):
-		im = Image.open(os.path.join(folder, img))
-		print im.size
-		# image = np.array(misc.imread(os.path.join(folder, img)), dtype=np.float32)
-		# print image
-		# image /= 256.0
-		# # print image.shape
-		# img_tensor = torch.from_numpy(image).permute(2, 0, 1)
-		# img_tensor = img_tensor.contiguous()
-		# print img_tensor.size()
-		normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-		transformer = transforms.Compose([transforms.Scale(256), transforms.CenterCrop(224), normalize, transforms.ToTensor()])
-		# img_tensor = transformer(img_tensor)
-		# img_pil = transforms.ToPILImage(img_tensor)
-		img_tensor = transformer(im)
-		print img_tensor.size()
-		try:
-			img_vgg =  vgg_model(autograd.Variable(torch.unsqueeze(img_tensor, dim=0), volatile=True))
-			print img_vgg
-			pickle.dump((img_vgg).data.numpy(), open(img, 'w'))
-		except Exception as ex:
-			print "DAMN -------------------- "
-			print ex
-		i += 1
-		if i == 3:
-			break
+    vgg_folder = folder + "_vgg"
+    i = 0
+    for index, image_filename in enumerate(os.listdir(folder)):
+        print('begin loop')
+        image = io.imread(os.path.join(folder, image_filename))
+        transformer = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Scale(256),
+            transforms.CenterCrop(224), 
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+        img_tensor = transformer(image)
+        assert tuple(img_tensor.size()) == (3, 224, 224)
+        try:
+            img_vgg = vgg_model(autograd.Variable(torch.unsqueeze(img_tensor, dim=0), volatile=True))
+            postvgg_filename = "{}.dat".format(image_filename)
+            postvgg_pathname = os.path.join(vgg_folder, postvgg_filename)
+            pickle.dump(img_vgg.data.numpy(), open(postvgg_pathname, 'wb'))
+            print('wrote the {}th image to the file {}'.format(index, postvgg_pathname))
+        except Exception as ex:
+            print("DAMN -------------------- ")
+            print(ex)
+            i += 1
+            if i == 3:
+                break
 
 get_vgg("../Data/train2014")
