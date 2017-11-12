5e36e804e9f86ef9aaa98b36b5ee5f7414f3b4fc
Praveen Kulkarni <praveenkulkarni1996@gmail.com>
2017-11-12 20:19:38 +0530

    [bugfix] add image id to vgg filenames

--- getVgg.py
+++ getVgg.py
@@ -34,22 +34,32 @@ def take_input(data_folder):
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
     vqa_image_dataset = datasets.ImageFolder(
-        root=data_folder, 
-        transform=transformer)
+        root=data_folder,
+        transform=transformer
+    )
     data_loader = DataLoader(dataset=vqa_image_dataset, 
-            batch_size=BATCH_SIZE, 
-            shuffle=True,
+            batch_size=BATCH_SIZE,
+            shuffle=False,
             num_workers=8)
+    """
+    for i, x in enumerate(vqa_image_dataset):
+        print(x)
+        break
+    """
     return data_loader
 
 def get_vgg(folder, data_loader):
+    print("DONT CALL THIS")
+    return
     """ writes the vgg feature vector """
     new_folder = "{}_vgg".format(folder)
     print('replace inputs to inputs.cuda() on HPC')
     for batch_index, (inputs, label) in enumerate(data_loader):
         print('here')
         inputs = autograd.Variable(inputs, volatile=True)
+        print('get_vgg', inputs)
         outputs = vgg_model(inputs)
+        print(inputs)
         # inputs = autograd.Variable(inputs.cuda(), volatile=True)
         # output = vgg_model(autograd.Variable(torch.unsqueeze(inputs, dim=0), volatile=True))
         # output = vgg_model(inputs)
@@ -63,6 +73,8 @@ def get_vgg(folder, data_loader):
 def depreciated(folder):
     vgg_folder = folder + "_vgg"
     mistakes = 0
+    batch = []
+    filenames = []
     for index, image_filename in enumerate(os.listdir(folder)):
         try:
             image = io.imread(os.path.join(folder, image_filename))
@@ -73,23 +85,34 @@ def depreciated(folder):
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             ])
-            img_tensor = transformer(image)
-            assert tuple(img_tensor.size()) == (3, 224, 224)
-            img_vgg = vgg_model(autograd.Variable(torch.unsqueeze(img_tensor, dim=0), volatile=True))
-            postvgg_filename = "{}.pkl".format(image_filename)
-            postvgg_pathname = os.path.join(vgg_folder, postvgg_filename)
-            pickle.dump(img_vgg.data.numpy(), open(postvgg_pathname, 'wb'))
-            print('wrote the {}th image to the file {}'.format(index, postvgg_pathname))
+            img_tensor = torch.unsqueeze(transformer(image), dim=0)
+            batch.append(img_tensor)
+            filenames.append(image_filename)
+            if len(batch) == BATCH_SIZE:
+                input_batch = torch.cat(batch) 
+                input_filenames = filenames
+                batch = []
+                filenames = []
+            else:
+                continue
+            outputs = vgg_model(autograd.Variable(input_batch, volatile=True))
+            for local_index, output_vector in enumerate(outputs.chunk(BATCH_SIZE, dim = 0)):
+                filename = '{}.pkl'.format(input_filenames[local_index])
+                pathname = os.path.join(vgg_folder, filename)
+                with open(pathname, 'wb') as fin:
+                    pickle.dump(output_vector.data.numpy(), fin)
+                print('written {}'.format(pathname))
         except Exception as ex:
-            print("Screwup here -------------------- ")
+            print("Screwup @ {}  -------------------- ".format(image_filename))
             print(ex)
             mistakes += 1
-            if True and mistakes == 3: 
-                break
+            if False and mistakes == 3: break
+        print('mistakes = {}', mistakes)
 
 if __name__ == '__main__':
     print("Please set folder correctly")
     folder = "../Data/train2014"
-    data_loader = take_input(data_folder = folder)
-    get_vgg(folder = folder, data_loader = data_loader)
+    # data_loader = take_input(data_folder = folder)
+    # get_vgg(folder = folder, data_loader = data_loader)
+    depreciated(folder = folder)
     # get_vgg("/scratch/cse/btech/cs1140485/DL_Course_Data/train2014")
