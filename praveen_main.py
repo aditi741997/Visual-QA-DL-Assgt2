6e8b3275df505e9a0d4cceea3e65e4822245f311
Praveen Kulkarni <praveenkulkarni1996@gmail.com>
2017-11-24 20:17:14 +0530

    add SAN

---
+++ praveen_main.py
@@ -0,0 +1,146 @@
+import os
+import time
+import argparse
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch.optim import lr_scheduler
+from torch.utils.data import ConcatDataset
+from torch.utils.data import DataLoader
+
+from praveen_dataset import VQA_Dataset
+from praveen_stacked import Stacked_Attention_VQA
+from vqa_base import VQA_Baseline
+
+# Global variables
+path = "/scratch/cse/btech/cs1140485/DL_Course_Data/"
+GPU = torch.cuda.is_available()
+validation_batch_limit = 500
+
+def process_data(data):
+  images, questions, answers = data
+  images = Variable(torch.squeeze(images, dim=0))
+  questions = Variable(torch.squeeze(questions, dim=0))
+  answers = Variable(torch.squeeze(answers, dim=0))
+  if GPU:
+    images = images.cuda()
+    questions = questions.cuda()
+    answers = answers.cuda()
+  return images, questions, answers
+
+def get_accuracy(model, dataloader):
+  t1 = time.time()
+  right, total, unknown = 0, 0, 0
+  for i, data in enumerate(dataloader):
+    images, questions, answers = process_data(data)
+    outputs = model(images, questions)
+    _, predicts = torch.max(outputs, 1)
+    try:
+      assert(predicts.size(0) == answers.size(0))
+      total += predicts.size(0)
+      right += (predicts == answers).sum().data[0]
+    except Exception as ex:
+      print ex
+    unknown += len(answers[answers == -1])
+    if i == validation_batch_limit:
+      break
+  print("Validation Time {}".format(time.time()-t1))
+  return right, total, unknown, time.time()-t1
+
+def train(model, args, train_dataset, test_dataset):
+  log = "lr {}, mtm {}, wt decay {}, gamma {}, activation {}, save_path {}".format(args.learning_rate, args.momentum, args.weight_decay, args.gamma, args.activation_fn, args.model_save_path)
+  if args.model_load_path:
+    log += "\nLoad model from"+args.model_load_path
+  print log
+  args.log.write(log+"\n")
+  # Dataloaders
+  train_dataloader = DataLoader(train_dataset, shuffle=True, num_workers=args.num_workers)
+  test_dataloader = DataLoader(test_dataset, shuffle=True, num_workers=args.num_workers)
+
+  # Loss fn, optimizer, scheduler
+  loss = nn.CrossEntropyLoss(ignore_index=-1)
+  optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=args.learning_rate, momentum=args.momentum, weight_decay=args.weight_decay)
+  scheduler = lr_scheduler.ExponentialLR(optimizer, gamma=args.gamma)
+
+  if GPU:
+    model = model.cuda()
+
+  for epoch in xrange(args.num_epoch):
+    test_right, test_total, test_unknown, test_time = get_accuracy(model, test_dataloader)
+    t1 = time.time()
+    scheduler.step()
+    torch.save(model, args.model_save_path)
+    train_right, train_total, train_unknown = 0, 0, 0
+    total_loss = 0
+    print('just before')
+    for i, data in enumerate(train_dataloader):
+      # this is a batch of image-question pairs.
+      images, questions, answers = process_data(data)
+      print('just inside')
+      outputs = model(images, questions)
+      print('just outside', outputs)
+      _, predicts = torch.max(outputs, 1)
+      train_total += predicts.size(0)
+      train_right += (predicts == answers).sum().data[0]
+      train_unknown += len(answers[answers == -1])
+      
+      optimizer.zero_grad()
+      batch_loss = loss(outputs, answers)
+      total_loss += batch_loss.data[0]
+      batch_loss.backward()
+      optimizer.step()
+    t2 = time.time()
+    log = "Epoch {}, loss {}, train_acc {}, test_acc {}, reduced_train_acc {}, reduced_test_acc {}, time {}, test_time {}".format(
+      epoch, 
+      total_loss,
+      100.0*train_right/train_total, 
+      100.0*test_right/test_total, 
+      100.0*(train_right)/(train_total-train_unknown), 
+      100.0*(test_right)/(test_total-test_unknown), 
+      t2-t1,
+      test_time)
+    args.log.write(log+"\n")
+    args.log.flush()
+    print(log)
+    print("Epoch {} train {} {} {} test {} {} {}".format(epoch, train_right, train_total, train_unknown, test_right, test_total, test_unknown))
+
+
+def get_arguments():
+  # ques params
+  parser = argparse.ArgumentParser(description='VQA_Base')
+  parser.add_argument("--activation-fn", type=str, default="tanh")
+  parser.add_argument("--question-hidden-dim", type=int, default=512)
+
+  parser.add_argument("--learning-rate", type=float, default=0.01)
+  parser.add_argument("--momentum", type=float, default=0.9)
+  parser.add_argument("--weight-decay", type=float, default=0.001)
+  parser.add_argument("--gamma", type=float, default=0.96)
+  parser.add_argument("--cell-type", type=str, default="lstm")
+
+  parser.add_argument("--num-epoch", type=int, default=100)
+  parser.add_argument("--batch-size", type=int, default=256)
+  parser.add_argument("--num-workers", type=int, default=32)
+
+  parser.add_argument("--model-save-path", type=str, default="model.pth")
+  parser.add_argument("--model-load-path", type=str)
+  parser.add_argument("--log", type=str, default="log.txt")
+  args = parser.parse_args()
+
+  args.model_save_path = os.path.join("experiments", args.model_save_path)
+  args.log = open(os.path.join("experiments", args.log), "w")
+  return args
+
+def main(args):
+  if args.model_load_path:
+    model = torch.load(args.model_load_path)
+  else:
+    print('right path')
+    model = Stacked_Attention_VQA() #defaults
+  train_dataset = VQA_Dataset(path, "train2014", args.batch_size)
+  # val_dataset = VQA_Dataset(path, "val2014", args.batch_size)
+  print('data set loaded')
+  train(model, args, train_dataset, train_dataset)
+
+if __name__ == '__main__':
+  args = get_arguments()
+  main(args)
---
+++ praveen_stacked.py
@@ -0,0 +1,128 @@
+# Based on https://arxiv.org/pdf/1511.02274.pdf
+import argparse
+import os
+import praveen_dataset
+import sys
+import torch
+import torch.nn as nn
+import torch.nn.functional as F 
+import torchvision.models as models
+import torch.autograd as autograd
+from torch.autograd import Variable
+from torchvision import transforms, datasets
+from skimage import io
+print("Imports completed")
+
+# vgg_model = models.vgg16(pretrained=True)
+# modified_pretrained = nn.Sequential(*list(vgg_model.features.children())[:-4]) 
+is_cuda = torch.cuda.is_available()
+
+class ImageEmbeddingExtractor(nn.Module):
+    """ Extract features from vgg """
+    
+    def __init__(self, output_size=1024):
+        super(ImageEmbeddingExtractor, self).__init__()
+        self.vgg = models.vgg16(pretrained=True)
+        self.vgg = nn.Sequential(*list(self.vgg.features.children())[:-4]) 
+        for parameter in self.vgg.parameters():
+            parameter.requires_grad = False
+        self.fc = nn.Sequential(nn.Linear(512, output_size), nn.Tanh())
+        self.type = torch.cuda.FloatTensor if is_cuda else torch.FloatTensor
+
+    def forward(self, image):
+        #vgg: (N, 224, 224) -> (N, 512, 14, 14)
+        features = self.vgg(image) 
+        features = features.view(-1, 512, 196).transpose(1, 2) #
+        embedding = self.fc(features)
+        return embedding
+
+class QuestionEmbeddingExtractor(nn.Module):
+    def __init__(self, input_size=300, output_size=1024, batch_first=True):
+        super(QuestionEmbeddingExtractor, self).__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self.ques_lstm_1 = nn.LSTM(input_size, output_size, num_layers=1, bidirectional=False, batch_first=True)
+        self.type = torch.cuda.FloatTensor if is_cuda else torch.FloatTensor
+
+    def forward(self, questions):
+        batch_sz = questions.data.size()[0]
+        h0 = autograd.Variable(torch.randn(1, batch_sz, self.output_size))
+        c0 = autograd.Variable(torch.randn(1, batch_sz, self.output_size))
+        out_ques_1, (hidden_ques_1, c_ques_1) = self.ques_lstm_1(questions,(h0, c0))
+        return hidden_ques_1[0]
+
+
+
+class Attention(nn.Module):
+    def __init__(self, d=1024, k=512, dropout=True):
+        super(Attention, self).__init__()
+        self.image_ff = nn.Linear(d, k)
+        self.quest_ff = nn.Linear(d, k)
+        self.attention_ff = nn.Linear(k, 1)
+        self.type = torch.cuda.FloatTensor if is_cuda else torch.FloatTensor
+
+    def forward(self, vI, vQ):
+        hI = self.image_ff(vI)  # [N, 196, 1024] -> [N, 196, 512]
+        hQ = self.quest_ff(vQ).unsqueeze(dim=1) # [N, 1024] -> [N, 512] -> [N, 1, 512]
+        hA = F.tanh(hI + hQ)    # [N, 196, 512] 
+        hA = self.attention_ff(hA).squeeze(dim=2) # [N, 196, 512] -> [N, 196, 1] -> [N, 196]
+        p = F.softmax(hA)
+        vI_new = (p.unsqueeze(dim=2) * vI).sum(dim = 1) # ([N, 196, 1], [N, 196, 1024]) -> [N, 1024]
+        vQ_new = vI_new + vQ
+        return vQ_new
+
+
+class Stacked_Attention_VQA(nn.Module):
+    
+    def __init__(self, 
+            image_embedding_size=1024, 
+            word_embedding_size=300, 
+            question_embedding_size = 1024, 
+            output_size=1000, 
+            num_attention_layers=1):
+        super(Stacked_Attention_VQA, self).__init__()
+        self.image_embed = ImageEmbeddingExtractor(output_size=image_embedding_size)
+        self.quest_embed = QuestionEmbeddingExtractor(
+                input_size=word_embedding_size, 
+                output_size=question_embedding_size)
+        self.attention_stack = nn.ModuleList([
+            Attention(
+                d=image_embedding_size, k=word_embedding_size
+        )] * num_attention_layers)
+        self.ml_perceptron = nn.Sequential(nn.Dropout(p=0.5), nn.Linear(image_embedding_size, output_size))
+        self.type = torch.cuda.FloatTensor if is_cuda else torch.FloatTensor
+
+
+    def forward(self, images, questions):
+        image_embeddings = self.image_embed(images)
+        quest_embeddings = self.quest_embed(questions)
+        vI = image_embeddings
+        vQ = quest_embeddings
+        for attention_layer in self.attention_stack:
+            vQ = attention_layer(vI, vQ)
+        answer = self.ml_perceptron(vQ)
+        return answer
+
+
+def main():
+    args = get_arguments()
+    path = "/scratch/cse/btech/cs1140485/DL_Course_Data/"
+    train_data_loader = praveen_dataset.VQA_Dataset(
+            path=path, 
+            loc="train2014", 
+            batch_size=2)
+    san = Stacked_Attention_VQA()
+    for data in train_data_loader:
+        images, questions, answers = data
+        questions = autograd.Variable(torch.squeeze(questions, dim=0), volatile=True)
+        images = autograd.Variable(images, volatile=True)
+        outputs = san(images, questions)
+        print(outputs)
+        break
+    print("it works!!")
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+    print("This shouldn't be running")
+    main()
