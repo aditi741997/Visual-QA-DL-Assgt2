a3b3a4c3862357234669fe3310ef5e34d7547177
Aditi <cs1140205@iitd.ac.in>
2017-11-26 04:06:32 +0530

    tester part 2 final

--- tester.py
+++ tester.py
@@ -1,13 +1,14 @@
 import torch
 import json
-
+import pickle
+from torch.utils.data import DataLoader
 from praveen_dataset import VQA_Dataset
 from main import process_data
 
-data_path = "/scratch/"
-model_path = "??"
-loc = "??"
-batch_size = 128
+data_path = "/scratch/cse/btech/cs1140485/DL_Course_Data"
+model_path = "/home/cse/btech/cs1140485/DeepLearning/Assignment2/Visual-QA-DL-Assgt2/experiments/model_1_gru_1000.pth"
+loc = "test2015"
+batch_size = 64
 GPU = torch.cuda.is_available()
 
 def get_answer_map():
@@ -19,11 +20,12 @@ def get_answer_map():
 
 def main():
   answer_map = get_answer_map()
+  print "Yo"
   model = torch.load(model_path)
   dataset = VQA_Dataset(data_path, loc, batch_size)
   dataloader = DataLoader(dataset, num_workers=16)
   results_txt = "["
-  out_file = open("results.json", 'w')
+  out_file = open("/home/cse/btech/cs1140205/DL_Assgt2/new/" + "results_2.json", 'w')
   for i, data in enumerate(dataloader):
     images, questions, q_ids = process_data(data)
     outputs = model(images, questions)
@@ -33,10 +35,11 @@ def main():
       result_obj["question_id"] = q_ids.data[i]
       result_obj["answer"] = answer_map[predicts.data[i]]
       results_txt += (json.dumps(result_obj)) + ","
+    print "Batch ", i
   print "Done"
   results_txt += "]"
   out_file.write(results_txt)
 
 
-if _name_ == '_main_':
+if __name__ == '__main__':
   main()
